d907b07a7a8586357d4a09c6eddb1ee455a94b5e
Oğuzhan <oguzhanturgut611@gmail.com>
2024-04-07 11:07:28 +0300

    aetup

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,3 +1,30 @@
+<script setup>
+import {computed} from '@vue/reactivity';
+import {onMounted, ref} from "vue";
+import {useRoute} from "vue-router";
+import store from "../store";
+
+const route = useRoute();
+const keyword = ref("");
+const meals = computed(() => store.state.searchedMeals);
+
+function searchMeals() {
+  if (keyword.value) {
+    store.dispatch("searchMeals", keyword.value);
+  } else {
+    //store.commit("setSearchedMeals", []);
+  }
+}
+
+onMounted(() => {
+  keyword.value = route.params.name
+  if (keyword.value) {
+    searchMeals()
+  }
+})
+
+</script>
+
 <template>
   <div class="p-8 pb-0">
     <h1 class="text-4xl font-bold mb-4 text-orange-500">Search Meals by Name</h1>
@@ -26,32 +53,7 @@
   </div>
 </template>
 
-<script setup>
-import {computed} from '@vue/reactivity';
-import {onMounted, ref} from "vue";
-import {useRoute} from "vue-router";
-import store from "../store";
 
-const route = useRoute();
-const keyword = ref("");
-const meals = computed(() => store.state.searchedMeals);
-
-function searchMeals() {
-  if (keyword.value) {
-    store.dispatch("searchMeals", keyword.value);
-  } else {
-    //store.commit("setSearchedMeals", []);
-  }
-}
-
-onMounted(() => {
-  keyword.value = route.params.name
-  if (keyword.value) {
-    searchMeals()
-  }
-})
-
-</script>
 
 <style lang="scss" scoped>
 
